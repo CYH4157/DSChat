1a4336c6bb701651ca511a8dbef46a20f46efe3d
CYH4157 <47914438+CYH4157@users.noreply.github.com>
2024-05-20 10:14:36 +0800

    Update run_1.3b.sh

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
@@ -15,8 +15,18 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 
+#deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-1.3b \
+#   --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
+#   --enable_tensorboard \
+#   --tensorboard_path $OUTPUT \
+#   --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
+
+
 deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-1.3b \
-   --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
-   --enable_tensorboard \
-   --tensorboard_path $OUTPUT \
-   --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
+    --gradient_accumulation_steps 8 --gradient_checkpointing --lora_dim 128 --zero_stage $ZERO_STAGE \
+    --num_train_epochs 2 \
+    --per_device_train_batch_size 6 \
+    --per_device_eval_batch_size 6 \
+    --enable_tensorboard \
+    --tensorboard_path $OUTPUT \
+    --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
